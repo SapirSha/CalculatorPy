c04efa4105b5cf98304532b7b8e14dcae75e364b
sapir <sapirsha10@gmail.com>
2024-12-05 03:11:44 -0800

    Basic Stack

---
+++ Stack.py
@@ -0,0 +1,23 @@
+class StackType:
+    def __init__(self, info = None, next = None):
+        self.info = info
+        self.next = next
+
+
+class Stack:
+    def __init__(self, stack = StackType()):
+        self.stack = stack
+
+    def is_empty(self):
+        return self.stack.info is None
+
+    def push(self, info):
+        self.stack = StackType(info, self.stack)
+
+    def pop(self):
+        t = self.stack.info
+        self.stack = self.stack.next
+        return t
+
+    def peek(self):
+        return self.stack.info
