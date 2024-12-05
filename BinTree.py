c2e9168849ebb64de8c9069dfbc7a2332a2eb39b
sapir <sapirsha10@gmail.com>
2024-12-05 03:54:01 -0800

    Tree type checks

--- BinTree.py
+++ BinTree.py
@@ -1,5 +1,8 @@
 class BinTree:
     def __init__(self, info, left = None, right = None):
+        if (type(left) != BinTree and left is not None)\
+                or (type(right) != BinTree and right is not None):
+            print("\t --- WARNING - TREE INITIALIZED WITH NON TREE CHILDREN ---Ty")
         self._left = left
         self._info = info
         self._right = right
@@ -7,8 +10,14 @@ class BinTree:
     def set_left(self, info, left = None, right = None):
         self._left = BinTree(info, left, right)
 
+    def set_left_tree(self, tree : 'BinTree'):
+        self._left = tree
+
     def set_right(self, info, left = None, right = None):
         self._right = BinTree(info, left, right)
+
+    def set_right_tree(self, tree : 'BinTree'):
+        self._right = tree
 
     def set_info(self, info):
         self._info = info
