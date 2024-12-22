82a9ef9f8e0067f9017dcc2d102f1f1af0498bc2
sapir <sapirsha10@gmail.com>
2024-12-22 10:18:06 -0800

    Adding EOF, ValueError, Memmory and unicode handlers

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from MakeTree.MakeTree import parentheses_stack
 from SolveEquation import solve_equation
 
 def ui_sort_of():
@@ -10,6 +9,19 @@ def ui_sort_of():
         res =  solve_equation(inp)
 
         return res
+    except UnicodeError:
+        print("<--- Error ------------------------------>")
+        print("Error decoding input!")
+        print("<---------------------------------------->")
+        return
+    except EOFError:
+        raise KeyboardInterrupt
+    except MemoryError:
+        print("<--- Error ------------------------------>")
+        print("Equation too big!")
+        print("<---------------------------------------->")
+        return
+    # just forget about all the exceptions raised in solve equation
     except Exception:
         return
 
@@ -21,7 +33,12 @@ def main():
         while True:
             res = ui_sort_of()
             if res is not None:
-                print(f"Answer: {res}")
+                try:
+                    print(f"Answer: {res}")
+                except ValueError:
+                    print("<--- Error ------------------------------>")
+                    print("Value too big to print")
+                    print("<---------------------------------------->")
     except KeyboardInterrupt:
         pass
 
